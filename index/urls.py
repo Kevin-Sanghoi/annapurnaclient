baee546661a1c3600f9494d627197d58bf9ac7a1
AMANVerma28 <amansingh.v15@iiits.in>
2017-09-14 10:27:36 +0530

    Update urls.py

--- index/urls.py
+++ index/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^maps', views.index1, name='index1'),
     url(r'^well', views.well, name='well'),
     url(r'^household', views.household, name='household'),
+    url(r'^landlord', views.landlord, name='landlord'),
     url(r'^farms', views.farms, name='farms'),
 ]
